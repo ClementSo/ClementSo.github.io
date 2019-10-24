7f25fccb7c00e9bf1738161f5123102e33dae333
Clement <csortino@edenschool.com>
2019-10-24 17:32:52 +0200

    Updates

---
+++ precache-manifest.883f6bdf0fc39edaed57eb4656dfdba3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4a59047c288a44a612e2cbca9acade8",
+    "url": "/mastermind/index.html"
+  },
+  {
+    "revision": "4b215722b273613b1b00",
+    "url": "/mastermind/static/css/main.c8c7d9e5.chunk.css"
+  },
+  {
+    "revision": "a165f6070fb2fd6062a4",
+    "url": "/mastermind/static/js/2.1b575431.chunk.js"
+  },
+  {
+    "revision": "4b215722b273613b1b00",
+    "url": "/mastermind/static/js/main.e42ccee0.chunk.js"
+  },
+  {
+    "revision": "32639ebdd51befc465c9",
+    "url": "/mastermind/static/js/runtime-main.0a89982d.js"
+  }
+]);
